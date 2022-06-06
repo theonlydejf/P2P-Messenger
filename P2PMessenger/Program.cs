cce7517c24d1c6f34481d53758e23b5c5e0a60f9
theonlydejf <krcmar.dav@gmail.com>
2022-06-06 08:41:42 +0200

    added P2P lib

--- P2PMessenger/Program.cs
+++ P2PMessenger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace P2PMessenger
 {
@@ -6,7 +7,20 @@ namespace P2PMessenger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(GetBroadCastIP(IPAddress.Parse("localhost"), ));
+            Console.ReadLine();
+        }
+
+        static IPAddress GetBroadCastIP(IPAddress host, IPAddress mask)
+        {
+            byte[] broadcastIPBytes = new byte[4];
+            byte[] hostBytes = host.GetAddressBytes();
+            byte[] maskBytes = mask.GetAddressBytes();
+            for (int i = 0; i < 4; i++)
+            {
+                broadcastIPBytes[i] = (byte)(hostBytes[i] | (byte)~maskBytes[i]);
+            }
+            return new IPAddress(broadcastIPBytes);
         }
     }
 }
